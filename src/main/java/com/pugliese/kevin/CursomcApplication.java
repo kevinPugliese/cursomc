8209e66372997a998f06ea38a3b903908a8af49f
Kevin Pugliese <kevin.lima.contato@gmail.com>
2018-08-14 12:23:35 -0300

    Criando operação de instancia

--- src/main/java/com/pugliese/kevin/CursomcApplication.java
+++ src/main/java/com/pugliese/kevin/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.pugliese.kevin;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pugliese.kevin.domain.Categoria;
+import com.pugliese.kevin.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRespository.save(Arrays.asList(cat1, cat2));
 	}
 }
