41923bb33a40e083cda49da97804ec78eee7c5d4
Kevin Pugliese <kevin.lima.contato@gmail.com>
2018-08-16 19:27:33 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/pugliese/kevin/services/execeptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.pugliese.kevin.services.execeptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
